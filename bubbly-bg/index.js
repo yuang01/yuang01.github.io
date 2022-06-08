1110ea6b113984ea47c22a61e12f7b8abbea2b4e
yuang01 <1730241541@qq.com>
2022-06-08 17:53:24 +0800

    Site updated: 2022-06-08 17:53:22

---
+++ bubbly-bg/index.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    bubbly({
+        colorStart: '#fff4e6',
+        colorStop: '#ffe9e4',
+        blur:1,
+        compose: 'source-over',
+        bubbleFunc:() => `hsla(${Math.random() * 50}, 100%, 50%, .3)`
+      });
+}
